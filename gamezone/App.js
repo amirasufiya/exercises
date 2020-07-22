894413dd68611743e2158d2a083df18527bf3b86
amirasufiya <amira.sufiya@live.iium.edu.my>
2020-07-23 01:36:55 +0800

    up until basic drawer #24

--- gamezone/App.js
+++ gamezone/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'react-native-gesture-handler';
 import Home from './screens/home';
 import About from './screens/about';
 import ReviewDetails from './screens/reviewDetails';
-import Navigator from './routes/homeStack';
+import Navigator from './routes/drawer';
 
 export default function App() {
   return (
--- gamezone/routes/homeStack.js
+++ gamezone/routes/homeStack.js
@@ -1,5 +1,4 @@
 import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
 import Home from '../screens/home';
 import ReviewDetails from '../screens/reviewDetails';
 
@@ -7,15 +6,13 @@ const screens = {
   Home: {
     screen: Home,
     navigationOptions: {
-      title: 'Game Zone',
-      // headerStyle: { backgroundColor: '#eee'}
+      title: 'GameZone',
     }
   },
   ReviewDetails: {
     screen: ReviewDetails,
     navigationOptions: {
       title: 'Review Details',
-      // headerStyle: { backgroundColor: 'red'}
     }
   },
 };
@@ -23,9 +20,9 @@ const screens = {
 // home stack navigator screens
 const HomeStack = createStackNavigator(screens, {
   defaultNavigationOptions: {
-    headerTintColor: '#444', //color of fonts
-    headerStyle: { backgroundColor: '#eee', height: 60},
+    headerTintColor: '#444',
+    headerStyle: { backgroundColor: '#eee', height: 60 }
   }
 });
 
-export default createAppContainer(HomeStack);
+export default HomeStack;
