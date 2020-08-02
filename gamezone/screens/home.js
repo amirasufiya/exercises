692058f8616ed3d5a9a14ba7ff564e8bc7a3bc96
amirasufiya <amira.sufiya@live.iium.edu.my>
2020-08-02 12:15:19 +0800

    final #35 completed

--- gamezone/screens/home.js
+++ gamezone/screens/home.js
@@ -1,8 +1,10 @@
 import React, {useState} from 'react';
-import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal } from 'react-native';
+import { StyleSheet, View, Text, FlatList, 
+  TouchableWithoutFeedback, TouchableOpacity, Modal, Keyboard } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import ReviewForm from '../screens/reviewForm';
 
 export default function Home({ navigation }) {
   
@@ -12,21 +14,32 @@ export default function Home({ navigation }) {
     { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
     { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
   ]);
+
+  const addReview = (review)  => {
+    review.key = Math.random().toString();
+    setReviews((currentReviews) => {
+      return [review, ...currentReviews]
+    });
+    setModalOpen(false);
+  }
   
   return (
     <View style={globalStyles.container}>
 
+
       <Modal visible={modalOpen} animationType='slide'>
-        <View style={styles.modalContent}>
-          <Icon 
-          name="close" 
-          type="material-icons" 
-          size={24} 
-          onPress={() => setModalOpen(false)} 
-          style={{...styles.modalToggle, ...styles.modalClose}}
-          />
-          <Text>Hello from the modal :)</Text>
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.modalContent}>
+            <Icon 
+            name="close" 
+            type="material-icons" 
+            size={24} 
+            onPress={() => setModalOpen(false)} 
+            style={{...styles.modalToggle, ...styles.modalClose}}
+            />
+            <ReviewForm addReview={addReview} />
+          </View>
+        </TouchableWithoutFeedback>
       </Modal>
 
       <Icon 
